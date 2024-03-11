05bd6a94c7ce62ca8aa4c62549e3858d20dc4ed5
ashir khan <aaashir128@gmail.com>
2024-03-11 17:04:00 +0500

    test, flashcaards

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const LoadingScreen = React.lazy(
   () => import("../pages/LoadingScreen/LoadingScreen")
@@ -25,34 +26,55 @@ const App = React.lazy(() => import("../App"));
 
 const ProjectRoutes = () => {
   // const { localeBasics } = useLocale();
+  const theme = createTheme({
+    typography: {
+      fontFamily: "Inter",
+    },
+    // palette: {
+    //   primary: { main: "#1A43C0" },
+    //   white: { main: "#ffff" },
+    //   yellow: { main: "yellow" },
+    // },
+    // components: {
+    //   MuiTextField: {
+    //     styleOverrides: {
+    //       root: {
+    //         backgroundColor: "green !important", // Add !important
+    //       },
+    //     },
+    //   },
+    // },
+  });
   return (
-    <React.Suspense fallback={<LoadingScreen />}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<AdminDashboardServices />} />
-          <Route path="/admin/login" element={<AdminLogin />} />
-          <Route path="/admin" element={<AdminDashboardServices />} />
-          <Route
-            path="/admin/professors"
-            element={<ProfessorManagementServices />}
-          />
-          <Route
-            path="/admin/students"
-            element={<StudentManagementServices />}
-          />
-          <Route
-            path="/admin/flashcards"
-            element={<FlashcardsManagementServices />}
-          />
-          <Route
-            path="/admin/questions"
-            element={<QuestionsManagementServices />}
-          />
+    <ThemeProvider theme={theme}>
+      <React.Suspense fallback={<LoadingScreen />}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<AdminDashboardServices />} />
+            <Route path="/admin/login" element={<AdminLogin />} />
+            <Route path="/admin" element={<AdminDashboardServices />} />
+            <Route
+              path="/admin/professors"
+              element={<ProfessorManagementServices />}
+            />
+            <Route
+              path="/admin/students"
+              element={<StudentManagementServices />}
+            />
+            <Route
+              path="/admin/flashcards"
+              element={<FlashcardsManagementServices />}
+            />
+            <Route
+              path="/admin/questions"
+              element={<QuestionsManagementServices />}
+            />
 
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Router>
-    </React.Suspense>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Router>
+      </React.Suspense>
+    </ThemeProvider>
   );
 };
 export default ProjectRoutes;
