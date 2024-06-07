55962b5cb10a22bce2f742cc0a75ae4f77443892
ashir khan <aaashir128@gmail.com>
2024-06-07 18:20:33 +0500

    exam api

--- src/components/LVL4_Organs/Header/types/index.d.ts
+++ src/components/LVL4_Organs/Header/types/index.d.ts
@@ -2,5 +2,5 @@ import { Control } from "react-hook-form";
 import { Class, DeckId, Tag } from "../../../../utils/constants/DataTypes";
 
 export type HeaderProps = {
-  showSkip: boolean;
+  showSkip?: boolean;
 };
